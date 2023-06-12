0d7fc8c7c04e4719e0608078033f89c8ecc10b1f
angel <63884758+AngelG8@users.noreply.github.com>
2023-06-12 04:25:04 -0500

    Fixed users not being able to log in

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -1,33 +1,32 @@
 import * as usersDao from "./users-dao.js";
 
-var currentUserVar;
 const AuthController = (app) => {
-    const register = async (req, res) => {
+    const register = (req, res) => {
         const username = req.body.username;
-        const user = await usersDao.findUserByUsername(username);
+        const user = usersDao.findUserByUsername(username);
         if (user) {
             res.sendStatus(409);
             return;
         }
-        const newUser = await usersDao.createUser(req.body);
-        currentUserVar = newUser;
+        const newUser = usersDao.createUser(req.body);
+        req.session["currentUser"] = newUser;
         res.json(newUser);
     };
 
-    const login = async (req, res) => {
+    const login = (req, res) => {
         const username = req.body.username;
         const password = req.body.password;
-        const user = await usersDao.findUserByCredentials(username, password);
+        const user = usersDao.findUserByCredentials(username, password);
         if (user) {
-            currentUserVar = user;
+            req.session["currentUser"] = user;
             res.json(user);
         } else {
             res.sendStatus(404);
         }
     };
 
-    const profile = async (req, res) => {
-        const currentUser = currentUserVar
+    const profile = (req, res) => {
+        const currentUser = req.session["currentUser"];
         if (!currentUser) {
             res.sendStatus(404);
             return;
@@ -41,7 +40,6 @@ const AuthController = (app) => {
     };
 
     const update = (req, res) => { };
-
 
     app.post("/api/users/register", register);
     app.post("/api/users/login", login);
